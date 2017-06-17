af9f3ca37b868300e4f1879a15001d073789a508
Johannes Schleger <johannes.schleger@tum.de>
2017-06-17 18:37:20 +0200

    use correct versioning for tls-capture-server

--- tls-capture-server/scripts/start.sh
+++ tls-capture-server/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar lib/tls-capture-server-1.0.jar
+java -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar lib/${project.name}-${project.version}.jar
