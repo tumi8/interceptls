a61ec48464f4699d10b88e8e0c7f98726b4c0908
devjos <github_11837948@feido.de>
2018-04-03 10:19:06 +0200

    revise db schema

--- tls-analysis-server/src/main/java/de/tum/in/net/services/PostgreSQLDatabaseService.java
+++ tls-analysis-server/src/main/java/de/tum/in/net/services/PostgreSQLDatabaseService.java
@@ -6,8 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.tum.in.net.model.DatabaseService;
 import de.tum.in.net.model.NetworkId;
@@ -17,11 +21,12 @@ import de.tum.in.net.session.SessionID;
 
 public class PostgreSQLDatabaseService implements DatabaseService {
 
+  private static final Logger log = LoggerFactory.getLogger(PostgreSQLDatabaseService.class);
   private final String NEW_SESSION =
-      "INSERT INTO SESSION (timestamp, interception, network_type, dns_ip, dns_mac, bssid, ssid) VALUES (now(), ?, ?::network, ?::INET, ?::macaddr, ?::macaddr, ?)";
+      "INSERT INTO SESSION (timestamp, interception, network_type, public_ip, dns_ip, dns_mac, bssid, ssid) VALUES (now(), ?, ?::network, ?::INET, ?::INET, ?::macaddr, ?::macaddr, ?)";
   private final String INSERT_RESULT =
-      "INSERT INTO RESULTS (session_id, test_id, client_ip, server_ip, client_sent, client_rec, server_sent, server_rec, target, target_port)"
-          + " VALUES (?, ?, ?::INET, ?::INET, ?, ?, ?, ?, ?, ?)";
+      "INSERT INTO RESULTS (session_id, test_id, server_ip, client_sent, client_rec, server_sent, server_rec, target, target_port)"
+          + " VALUES (?, ?, ?::INET, ?, ?, ?, ?, ?, ?)";
   private BasicDataSource bds = new BasicDataSource();
 
 
@@ -51,14 +56,13 @@ public class PostgreSQLDatabaseService implements DatabaseService {
                 c.prepareStatement(INSERT_RESULT, Statement.RETURN_GENERATED_KEYS);
             stmt.setLong(1, sessionId);
             stmt.setInt(2, testCounter);
-            stmt.setString(3, r.getServerResult().getDestination());
-            stmt.setString(4, r.getClientResult().getDestination());
-            stmt.setBytes(5, r.getClientResult().getSentBytesRaw());
-            stmt.setBytes(6, r.getClientResult().getReceivedBytesRaw());
-            stmt.setBytes(7, r.getServerResult().getSentBytesRaw());
-            stmt.setBytes(8, r.getServerResult().getReceivedBytesRaw());
-            stmt.setString(9, r.getHostAndPort().getHost());
-            stmt.setInt(10, r.getHostAndPort().getPort());
+            stmt.setString(3, r.getClientResult().getDestination());
+            stmt.setBytes(4, r.getClientResult().getSentBytesRaw());
+            stmt.setBytes(5, r.getClientResult().getReceivedBytesRaw());
+            stmt.setBytes(6, r.getServerResult().getSentBytesRaw());
+            stmt.setBytes(7, r.getServerResult().getReceivedBytesRaw());
+            stmt.setString(8, r.getHostAndPort().getHost());
+            stmt.setInt(9, r.getHostAndPort().getPort());
 
             stmt.executeUpdate();
           } else {
@@ -84,10 +88,20 @@ public class PostgreSQLDatabaseService implements DatabaseService {
         s.setBoolean(1, result.anyInterception());
         NetworkId network = result.getNetworkId();
         s.setString(2, network.getType().toString());
-        s.setString(3, network.getDnsIp());
-        s.setString(4, network.getDnsMac());
-        s.setString(5, network.getBssid());
-        s.setString(6, network.getSsid());
+
+        Set<String> publicIps = result.getClientServerResults().stream()
+            .map(r -> r.getServerResult().getDestination()).collect(Collectors.toSet());
+
+        if (publicIps.size() == 0) {
+          throw new SQLException("No public IP address found in test results.");
+        } else if (publicIps.size() > 1) {
+          log.warn("Found more than one public IP address.");
+        }
+        s.setString(3, publicIps.iterator().next());
+        s.setString(4, network.getDnsIp());
+        s.setString(5, network.getDnsMac());
+        s.setString(6, network.getBssid());
+        s.setString(7, network.getSsid());
         s.executeUpdate();
         ResultSet set = s.getGeneratedKeys();
         set.next();
