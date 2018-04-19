a65ac844a4c8cb9714ef011a6793c809dbc84029
devjos <github_11837948@feido.de>
2018-04-19 09:00:08 +0200

    openssl has some problems with chacha20, so do not use it atm

--- tls-capture/src/main/java/de/tum/in/net/server/FileTlsServerConfig.java
+++ tls-capture/src/main/java/de/tum/in/net/server/FileTlsServerConfig.java
@@ -70,7 +70,9 @@ public class FileTlsServerConfig implements TlsServerConfig {
       if (this.privateKey instanceof RSAKeyParameters) {
         log.info("Initialise server with RSA cipher suites");
         signatureAlg = SignatureAlgorithm.rsa;
-        this.cipherSuites = new int[] {CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+        this.cipherSuites = new int[] {
+            // CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256, some problems with openssl,
+            // see http://lists.squid-cache.org/pipermail/squid-users/2016-October/013125.html
             CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
             CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
             CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
@@ -93,7 +95,10 @@ public class FileTlsServerConfig implements TlsServerConfig {
       } else if (this.privateKey instanceof ECKeyParameters) {
         log.info("Initialise server with ECDSA cipher suites");
         signatureAlg = SignatureAlgorithm.ecdsa;
-        this.cipherSuites = new int[] {CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+        this.cipherSuites = new int[] {
+            // CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256, some problems with
+            // openssl, see
+            // http://lists.squid-cache.org/pipermail/squid-users/2016-October/013125.html
             CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
             CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
             CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
