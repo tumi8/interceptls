67e9552da8b67df5461f3739aa92bea84f252e6f
devjos <github_11837948@feido.de>
2018-04-19 09:44:52 +0200

    new certs

--- tls-capture/certs/create.sh
+++ tls-capture/certs/create.sh
@@ -6,12 +6,12 @@ openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 730 -subj "/C=DE/S
 
 #create rsa server cert
 openssl genrsa -out server-key-rsa.pem 2048
-openssl req -new -key server-key-rsa.pem -out server-cert-rsa.csr -subj "/C=DE/ST=BY/L=MUC/O=TUM/CN=net.in.tum.de"
+openssl req -new -config cert.conf -key server-key-rsa.pem -out server-cert-rsa.csr
 openssl x509 -req -days 730 -in server-cert-rsa.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-rsa.pem
 
 #create ec server cert
 openssl ecparam -name secp521r1 -genkey -noout -out server-key-ec.pem
-openssl req -new -key server-key-ec.pem -out server-cert-ec.csr -subj "/C=DE/ST=BY/L=MUC/O=TUM/CN=net.in.tum.de"
+openssl req -new -config cert.conf -key server-key-ec.pem -out server-cert-ec.csr
 openssl x509 -req -days 730 -in server-cert-ec.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-ec.pem
 
 #create chains
