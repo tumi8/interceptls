325798578157e96f3b190f01f53469643d7d4be1
devjos <github_11837948@feido.de>
2018-04-19 10:16:44 +0200

    new certs

--- tls-capture/certs/create.sh
+++ tls-capture/certs/create.sh
@@ -6,13 +6,13 @@ openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 730 -subj "/C=DE/S
 
 #create rsa server cert
 openssl genrsa -out server-key-rsa.pem 2048
-openssl req -new -config cert.conf -key server-key-rsa.pem -out server-cert-rsa.csr
-openssl x509 -req -days 730 -in server-cert-rsa.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-rsa.pem
+openssl req -config ssl.conf  -extensions req_ext -new -key server-key-rsa.pem -out server-cert-rsa.csr
+openssl x509 -extfile ssl.conf -extensions req_ext -req -days 730 -in server-cert-rsa.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-rsa.pem
 
 #create ec server cert
 openssl ecparam -name secp521r1 -genkey -noout -out server-key-ec.pem
-openssl req -new -config cert.conf -key server-key-ec.pem -out server-cert-ec.csr
-openssl x509 -req -days 730 -in server-cert-ec.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-ec.pem
+openssl req -config ssl.conf -new -key server-key-ec.pem -out server-cert-ec.csr
+openssl x509 -extfile ssl.conf -extensions req_ext -req -days 730 -in server-cert-ec.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert-ec.pem
 
 #create chains
 cat ca-cert.pem server-cert-rsa.pem > server-cert-rsa.pem.chain
