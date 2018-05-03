b8691d588d506b92728d10e30bc10ccf27a5922c
devjos <github_11837948@feido.de>
2018-05-03 12:34:34 +0200

    update version and about screen of tls-client-android

--- tls-client-android/app/src/main/java/de/tum/in/net/client/AboutActivity.java
+++ tls-client-android/app/src/main/java/de/tum/in/net/client/AboutActivity.java
@@ -2,6 +2,9 @@ package de.tum.in.net.client;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import de.tum.in.net.model.TlsConstants;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        final TextView general = findViewById(R.id.general);
+        general.append(" " + TlsConstants.TLS_INFORMATION_SERVER_URL);
+
+        final TextView versionTextView = findViewById(R.id.app_version);
+        versionTextView.setText("Version: " + BuildConfig.VERSION_NAME);
+
     }
 }
