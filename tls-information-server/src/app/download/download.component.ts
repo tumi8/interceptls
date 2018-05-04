2f871ce07114f570f19ea1fec7821627c2e28936
devjos <github_11837948@feido.de>
2018-05-04 09:12:59 +0200

    update download page

--- tls-information-server/src/app/download/download.component.ts
+++ tls-information-server/src/app/download/download.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent }   from '../app.component';
 
 @Component({
   selector: 'app-download',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadComponent implements OnInit {
 
-  constructor() { }
+  constructor(public rootComponent: AppComponent) { }
 
   ngOnInit() {
   }
