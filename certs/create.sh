4e62c9101be95c5ef7344fba1fc47f8bf1977d2e
devjos <github_11837948@feido.de>
2018-05-04 08:42:58 +0200

    added certs used for production

---
+++ certs/create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#------------------------------------------------
+# CA for analysis server, including two server certs (RSA & ECDSA)
+#------------------------------------------------
+#create ca
+openssl ecparam -name secp521r1 -genkey -noout -out ca-key.pem
+openssl req -new -config ca.conf -extensions v3_ca -x509 -key ca-key.pem -out ca-cert.pem -days 730 -subj "/C=DE/ST=BY/L=MUC/O=TUM/CN=RootCA"
+
+#create rsa server cert
+openssl genrsa -out analysis-server-key-rsa.pem 2048
+openssl req -config analysis-server.conf  -extensions req_ext -new -key analysis-server-key-rsa.pem -out analysis-server-cert-rsa.csr
+openssl x509 -extfile analysis-server.conf -extensions req_ext -req -days 730 -in analysis-server-cert-rsa.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out analysis-server-cert-rsa.pem
+
+#create ec server cert
+openssl ecparam -name secp521r1 -genkey -noout -out analysis-server-key-ec.pem
+openssl req -config analysis-server.conf -new -key analysis-server-key-ec.pem -out analysis-server-cert-ec.csr
+openssl x509 -extfile analysis-server.conf -extensions req_ext -req -days 730 -in analysis-server-cert-ec.csr -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out analysis-server-cert-ec.pem
+
+#create chains
+cat ca-cert.pem analysis-server-cert-rsa.pem > analysis-server-cert-rsa.pem.chain
+cat ca-cert.pem analysis-server-cert-ec.pem > analysis-server-cert-ec.pem.chain
+
+#create keystore
+openssl pkcs12 -export -out tls-rsa.p12 -in analysis-server-cert-rsa.pem -inkey analysis-server-key-rsa.pem -CAfile ca-cert.pem -chain -password pass:password
+openssl pkcs12 -export -out tls-ec.p12 -in analysis-server-cert-ec.pem -inkey analysis-server-key-ec.pem -CAfile ca-cert.pem -chain -password pass:password
+
