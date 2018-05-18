30fbe1a476890e5fa73746e3598ec10415511bca
devjos <github_11837948@feido.de>
2018-05-18 10:02:25 +0200

    prepare 1.0.0 release

---
+++ bumb-version.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+VERSION=$1
+
+
+if [ -z "${VERSION}" ]; then
+    echo "USAGE: ./bump-version.sh <version-code>"
+    exit 1
+fi
+
+echo "Setting the version to: $VERSION"
+##
+# java projects
+##
+mvn versions:set -DnewVersion=$VERSION
+
+##
+# android project
+##
+
+#set version number
+# -i option is not cross plattform :(
+sed "s/versionName .*/versionName $VERSION/" tls-client-android/app/build.gradle > tls-client-android/app/build.gradle.new
+mv tls-client-android/app/build.gradle.new tls-client-android/app/build.gradle
+
+#increment android version code
+perl -i -pe 's/(versionCode )(\d+)$/$1.($2+1)/e' tls-client-android/app/build.gradle
+
