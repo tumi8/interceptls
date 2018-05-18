8dc48e7e1d33b08caf644c886089ed7d37bf00a1
devjos <github_11837948@feido.de>
2018-05-18 08:34:26 +0200

    change advisors, supervisor to team and include myself

--- tls-information-server/src/app/footer/footer.component.ts
+++ tls-information-server/src/app/footer/footer.component.ts
@@ -8,10 +8,12 @@ import { AppComponent }   from '../app.component';
 })
 export class FooterComponent implements OnInit {
 
-  advisors = [
+  team = [
+    {name:'Johannes Schleger', link:'mailto:johannes.schleger@tum.de'},
     {name:'Jonas Jelten', link:'https://net.in.tum.de/members/jelten/'},
     {name:'Florian Wohlfart', link:'https://net.in.tum.de/members/wohlfart/'},
     {name:'Quirin Scheitle', link:'https://net.in.tum.de/members/scheitle/'},
+    {name:'Prof. Dr.-Ing. Georg Carle', link:'https://net.in.tum.de/members/carle/'},
   ];
   links = [
     {name:'FAQ', link:'faq'},
