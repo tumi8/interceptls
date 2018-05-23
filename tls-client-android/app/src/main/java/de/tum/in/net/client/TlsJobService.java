1171c1a85849b38dc51571cc06a63dc632f6b8c3
devjos <github_11837948@feido.de>
2018-05-23 07:58:55 +0200

    startForegroundService starting with oreo

--- tls-client-android/app/src/main/java/de/tum/in/net/client/TlsJobService.java
+++ tls-client-android/app/src/main/java/de/tum/in/net/client/TlsJobService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Johannes Schleger (johannes.schleger@tum.de)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import android.app.job.JobService;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,7 +61,12 @@ public class TlsJobService extends JobService {
         log.debug("onStartJob TLS Job Service");
 
         final Intent i = new Intent(this, TlsService.class);
-        startService(i);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //from oreo on startService throws an IllegalStateException, see https://developer.android.com/about/versions/oreo/android-8.0-changes
+            startForegroundService(i);
+        } else {
+            startService(i);
+        }
 
         return false;
     }
